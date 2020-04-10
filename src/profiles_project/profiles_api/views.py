2467b4cca601f66b4e2b11a4a17534f6eb2e2aca
pavan520 <kantamani.pavan520@gmail.com>
2020-04-10 18:51:10 +0530

    Added an simple API view and URl router

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
 
 # Create your views here.
+
+class HelloAPIView(APIView):
+    """ Test API View"""
+    def get(self,request, format=None):
+        """ Return list of Features of APIView"""
+        apiView =["Uses HTTP functions GET,POST,PUT,DELETE","It's a traditional Django View",
+                  "Gives you most control over logic," "Is manually mapped to ur URL's"]
+
+        return Response({"message":"Hello","features":apiView})
